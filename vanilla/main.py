844c77fbd5addbdab305cb8f4474257f6c77e784
SuitO <suitx.qb@gmail.com>
2025-03-31 02:42:06 +0200

    finishing DNN vanilla implementation and starting pytorch implementation

--- vanilla/main.py
+++ vanilla/main.py
@@ -26,7 +26,7 @@ class DNN:
 
     def sigmoid(self, x, derivative=False):
         if derivative:
-            return (np.exp(-x))/((np.xp(-x)+1)**2)
+            return (np.exp(-x))/((np.exp(-x)+1)**2)
         return 1/(1+np.exp(-x))
 
     def softmax(self, x, derivative=False):
@@ -45,11 +45,11 @@ class DNN:
         params['A1'] = self.sigmoid(params['Z1'])
 
         # hidden_layer_1 to hidden_layer_2
-        params['Z2'] = np.dot(params['W2'], params['A1'])  # 128x1
+        params['Z2'] = np.dot(params['W2'], params['A1'])
         params['A2'] = self.sigmoid(params['Z2'])
 
         # hidden_layer_2 to output_layer
-        params['Z3'] = np.dot(params['W3'], params['A2'])  # 128x1
+        params['Z3'] = np.dot(params['W3'], params['A2'])
         params['A3'] = self.softmax(params['Z3'])
 
         return params['Z3']
@@ -71,6 +71,8 @@ class DNN:
         error = np.dot(params['W2'].T, error) * self.sigmoid(params['Z1'], derivative=True)
         change_w['W1'] = np.outer(error, params['A0'])
 
+        return change_w
+
     def update_weights(self, change_w):
         for key, val in change_w.items():
             self.params[key] -= self.lr * val #W_t+1 = W_t - lr*Delta_W_t
@@ -79,26 +81,27 @@ class DNN:
         predictions = []
         for x in test_data:
             values = x.split(",")
-            inputs = (np.asarray(values[1:]) / 255.0 * 0.99) + 0.01
+            inputs = (np.asarray(values[1:], dtype=float) / 255.0 *0.99) + 0.01
             targets = np.zeros(10) + 0.01
+            targets[int(values[0])] = 0.99
             output = self.forward_pass(inputs)
             pred = np.argmax(output)
             predictions.append(pred==np.argmax(targets))
-
         return np.mean(predictions)
 
     def train(self, train_list, test_list):
         start_time = time.time()
         for i in range(self.epochs):
             for x in train_list:
-                values = x.plsit(",")
-                inputs = (np.asarray(values[1:]) /255.0 * 0.99) + 0.01
+                values = x.split(",")
+                inputs = (np.asarray(values[1:], dtype=float) / 255.0 * 0.99) + 0.01
                 targets = np.zeros(10) + 0.01
                 targets[int(values[0])] = 0.99
                 output = self.forward_pass(inputs)
                 change_w = self.backward_pass(targets, output)
                 self.update_weights(change_w)
             accuracy = self.compute_accuracy(test_list)
+            print('Epoch {0}, Time Spent: {1:.02f}s, Accuracy: {2:.2f}%'.format(i+1, time.time()-start_time, accuracy*100))
 
 train_file = open("/home/suito/Documents/Projet/CNN_MNIST/train.csv")
 train_list = train_file.readlines()
@@ -108,5 +111,5 @@ test_file = open("/home/suito/Documents/Projet/CNN_MNIST/test.csv")
 test_list = test_file.readlines()
 test_file.close
 
-dnn = DNN(sizes=[784, 128, 64, 10], epochs=10, lr=0.001)
+dnn = DNN(sizes=[784, 256, 128, 10], epochs=20, lr=0.001)
 dnn.train(train_list, test_list)
